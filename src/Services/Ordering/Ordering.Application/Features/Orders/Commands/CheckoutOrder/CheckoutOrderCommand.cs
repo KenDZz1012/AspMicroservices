c33879b90d4acdbf1087ad3ed4a7dcbc11f13106
KENDZ\kient <kienteo1012@gmail.com>
2023-02-07 00:18:16 +0700

    code new

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommand.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommand : IRequest<int>
+    {
+        public string UserName { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+
+        //BillingAddress
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string AddressLine { get; set; }
+
+        public string Country { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        //Payment
+
+        public string CardName { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public string Expiration { get; set; }
+
+        public string CVV { get; set; }
+
+        public int PaymentMethod { get; set; }
+       
+
+        public CheckoutOrderCommand(string userName, decimal totalPrice, string firstName, string lastName, string emailAddress, string addressLine, string country, string state, string zipCode, string cardName, string cardNumber, string expiration, string cVV, int paymentMethod)
+        {
+            UserName = userName;
+            TotalPrice = totalPrice;
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAddress;
+            AddressLine = addressLine;
+            Country = country;
+            State = state;
+            ZipCode = zipCode;
+            CardName = cardName;
+            CardNumber = cardNumber;
+            Expiration = expiration;
+            CVV = cVV;
+            PaymentMethod = paymentMethod;
+        }
+    }
+}
