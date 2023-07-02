9bae8206146a96a90482ac0e43911c40b7438afe
KENDZ\kient <kienteo1012@gmail.com>
2023-07-02 22:20:42 +0700

    code new

---
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Ordering.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<T>(this IHost host, Action<T,IServiceProvider> seeder,int? retry = 0) where T : DbContext
+        {
+            int retryForAvailability = retry.Value;
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<T>>();
+                var context = services.GetService<T>();
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context {DbContextName}");
+
+                    InvokeSeeder(seeder, context, services);
+
+                    logger.LogInformation("Migrated database associated with context {DbContextName}");
+                }
+                catch(SqlException ex)
+                {
+                    throw;
+                }
+            }
+            return host;
+
+        }
+
+        private static void InvokeSeeder<T>(Action<T, IServiceProvider> seeder, T context, IServiceProvider services) where T : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Persistence
 {
-    internal class OrderContextSeed
+    public class OrderContextSeed
     {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfigureOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database ");
+            }
+        }
+
+        public static IEnumerable<Order> GetPreconfigureOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName= "Ken",FirstName="Ng",LastName="ken",EmailAddress="kienteo1012@gmail.com",AddressLine="Ha noi",Country="Viet Nam",TotalPrice=200}
+            };
+        }
     }
 }
